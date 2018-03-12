c473cc6d1abcc707574c472a5d805839e813a62d
Ajay Sathish <u1068959@ad.utah.edu>
2018-03-11 20:52:08 -0600

    Generated nodes

---
+++ AISnakeGame/Assets/Scripts/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+
+    public bool walkable;
+    public Vector3 worldPosition;
+
+    public Node(bool i_walkable, Vector3 i_worldPos)
+    {
+        walkable = i_walkable;
+        worldPosition = i_worldPos;
+    }
+}
